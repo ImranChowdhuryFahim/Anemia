626a9f6e14b731254a50ccdeb28215768ce419d7
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-08-06 10:29:25 +0600

    fetching hemoglobin data from hosted model in heroku

---
+++ app/src/main/java/com/defense/anemiadetection/Hb.java
@@ -0,0 +1,13 @@
+package com.defense.anemiadetection;
+
+public class Hb {
+    public float hb;
+
+    public void setHb(float hb) {
+        this.hb = hb;
+    }
+
+    public float getHb() {
+        return hb;
+    }
+}
--- app/src/main/java/com/defense/anemiadetection/MainActivity.java
+++ app/src/main/java/com/defense/anemiadetection/MainActivity.java
@@ -7,38 +7,46 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Camera;
-import android.media.Image;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.defense.anemiadetection.ml.Model;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
-
-import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.support.image.TensorImage;
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
     private Button select,capture,predict;
     private ImageView image;
     private TextView tv;
     private Bitmap img;
+    private RadioButton male,female;
+    private ProgressDialog progressDialog;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         predict = findViewById(R.id.predict);
         image = findViewById(R.id.iv);
         tv = findViewById(R.id.tv);
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
 
         select.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,38 +92,90 @@ public class MainActivity extends AppCompatActivity {
         predict.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    img = Bitmap.createScaledBitmap(img,224,224,true);
-                    Model model = Model.newInstance(MainActivity.this);
 
+                    Bitmap croppedBitmap = Bitmap.createScaledBitmap(img,864,1152,true);
 
-                    TensorImage tensorImage = new TensorImage(DataType.UINT8);
-                    tensorImage.load(img);
+                    int r = 0;
+                    int g = 0;
+                    int b = 0;
 
-                    ByteBuffer byteBuffer = tensorImage.getBuffer();
+                    int total = 0;
 
-                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.UINT8);
-                    inputFeature0.loadBuffer(byteBuffer);
-                    // Runs model inference and gets result.
-                    Model.Outputs outputs = model.process(inputFeature0);
-                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-
-
-                    // Releases model resources if no longer used.
-                    model.close();
-
-                    if(outputFeature0.getFloatArray()[0] >= outputFeature0.getFloatArray()[1])
+                    int width = croppedBitmap.getWidth();
+                    int height = croppedBitmap.getHeight();
+                    for (int x = 0; x <width; x++)
                     {
-                        tv.setText("\nYes\n Anemia detected.");
-                    }
-                    else {
-                        tv.setText("\nNo Anemia");
+                        for (int y = 0; y < height; y++)
+                        {
+                            int c = croppedBitmap.getPixel(x,y);
+                            total++;
+                            r += Color.red(c);
+                            g += Color.green(c);
+                            b += Color.blue(c);
+
+                        }
                     }
 
+                    int avgRed = (r/total);
+                    int avgGreen = (g/total);
+                    int avgBlue = (b/total);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    int gender ;
+
+                    if(male.isChecked()) gender = 0;
+                    else gender = 1;
+
+
+
+
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Please wait..."); // Setting Message
+                progressDialog.setTitle("Validating"); // Setting Title
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER); // Progress Dialog Style Spinner
+                progressDialog.show(); // Display Progress Dialog
+                progressDialog.setCancelable(false);
+
+                if(isNetworkConnected())
+                {
+                    queue = Volley.newRequestQueue(MainActivity.this);
+                    String url = "https://anemia-detection-web-app.herokuapp.com/get-hemoglobin-result?r="+avgRed+"&g="+avgGreen+"&b="+avgBlue+"&gender="+gender;
+                    System.out.println(url);
+
+
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    progressDialog.dismiss();
+                                    Gson gson = new Gson();
+                                    Hb hb = gson.fromJson(response.toString(),Hb.class);
+//                                    System.out.println(hb.getHb());
+//                                    Log.d("D", "Himoglobin: "+hb.getHb());
+                                    tv.setText("Hemoglobin Level: "+hb.getHb());
+                                }
+                            }, new Response.ErrorListener() {
+
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    progressDialog.dismiss();
+                                    // TODO: Handle error
+
+                                }
+                            });
+
+
+                    queue.add(jsonObjectRequest);
                 }
+                else {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"No Internet",Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+
             }
         });
     }
@@ -121,6 +183,12 @@ public class MainActivity extends AppCompatActivity {
     private void capture() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent,200);
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(MainActivity.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 
     @Override
@@ -165,3 +233,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
